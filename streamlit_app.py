5b8fa2ac36d574a59a3b8301652fb196cc315cdd
mayra463 <mayratrihapsari@gmail.com>
2025-05-26 10:26:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈Hai selamat datang di web ini")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Belajar ngoding bareng mayra"
 )
