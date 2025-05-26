7b55a3a00917ad629a4f0a4c4f49e5da3811b5a2
mayra463 <mayratrihapsari@gmail.com>
2025-05-26 10:39:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈Hai selamat datang di web ini")
 st.write(
     "Belajar ngoding bareng mayra"
 )
-st.image("https://github.com/mayra463/Mayra/blob/main/IMG-20250526-WA0018.jpg", width=200) 
+st.image("IMG-20250526-WA0018.jpg", width=200) 
