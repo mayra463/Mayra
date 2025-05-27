849eba1d01a7b8e7a58580612cc41b3fe7bb121e
mayra463 <mayratrihapsari@gmail.com>
2025-05-27 12:06:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ st.write(
     "Belajar ngoding bareng mayra"
 )
 st.image("IMG-20250526-WA0018.jpg", width=200) 
+
+st.title("Aplikasi Sederhana") 
+st.header= st.("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1) 
+
+if (angka % 2) == 0:
+    st.write(f"(angka) adalah Bilangan Genap") 
+else :
+    st.write(f"(angka) adalah Bilangan Ganjil") -! 
